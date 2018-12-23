b5f055fe1a3dcc98aa589d020b148acb0f1aee88
Christianconnectitude <christian.falck@connectitude.se>
2018-12-23 23:39:10 +0100

    Added empty functions and fields for Game

--- EvenTheTeams/Game.cs
+++ EvenTheTeams/Game.cs
@@ -8,5 +8,38 @@ namespace EvenTheTeams
 {
     class Game
     {
+
+        private Player[] team1;
+        private Player[] team2;
+
+        public Game()
+        {
+
+        }
+
+        public Player[] Team1
+        {
+            get
+            {
+                return team1;
+            }
+            set
+            {
+                team1 = value;
+            }
+        }
+
+        public Player[] Team2
+        {
+            get
+            {
+                return team2;
+            }
+            set
+            {
+                team2 = value;
+            }
+        }
+
     }
 }
