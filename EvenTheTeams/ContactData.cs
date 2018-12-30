920928f2e8f5c7676dab3c1e6ac7717cfb69c3f2
Christianconnectitude <christian.falck@connectitude.se>
2018-12-30 20:57:29 +0100

    added Lenas version of ContactData.cs

---
+++ EvenTheTeams/ContactData.cs
@@ -0,0 +1,52 @@
+﻿//Lena Lindblad
+//HT2018-DA204B-88221
+//Project 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenTheTeams
+{
+    //Class for players email and phone
+    public class ContactData
+    {
+        private string email;
+        private string phone;
+
+        //Constructor
+        public ContactData()
+        {
+
+        }
+
+        public ContactData(string email, string phone)
+        {
+            Initialize();
+            this.email = email;
+            this.phone = phone;
+        }
+
+        //Properties
+        public string Phone
+        {
+            get { return phone; }
+
+            set { phone = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+
+            set { email = value; }
+        }
+        private void Initialize()
+        {
+            email = string.Empty;
+            phone = string.Empty;
+        }
+    }
+}
